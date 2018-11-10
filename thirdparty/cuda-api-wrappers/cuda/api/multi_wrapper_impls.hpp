8180191c1f7f91749c1e059e92594624edb040d6
Gawaboumga <yourihubaut@hotmail.com>
2018-11-10 17:21:46 +0100

    Add third party libraries Catch2 & cuda-api-wrappers

---
+++ thirdparty/cuda-api-wrappers/cuda/api/multi_wrapper_impls.hpp
@@ -0,0 +1,161 @@
+/**
+ * @file multi_wrapper_impls.hpp
+ *
+ * @brief Implementations of methods or functions requiring the definitions of
+ * multiple CUDA entity proxy classes. In some cases these are declared in the
+ * individual proxy class files, with the other classes forward-declared.
+ */
+#pragma once
+#ifndef MULTI_WRAPPER_IMPLS_HPP_
+#define MULTI_WRAPPER_IMPLS_HPP_
+
+#include <cuda/api/stream.hpp>
+#include <cuda/api/device.hpp>
+#include <cuda/api/event.hpp>
+
+namespace cuda {
+
+namespace event {
+
+/**
+ * @brief creates a new execution stream on a device.
+ *
+ * @note The CUDA API runtime defaults to creating
+ * which you synchronize on by busy-waiting. This function does
+ * the same for compatibility.
+ *
+ * @param device The device on which to create the new stream
+ * @param uses_blocking_sync When synchronizing on this new evet,
+ * shall a thread busy-wait for it, or
+ * @param records_timing Can this event be used to record time
+ * values (e.g. duration between events)
+ * @param interprocess Can multiple processes work with the constructed
+ * event?
+ * @return The constructed event proxy class
+ */
+template <bool DeviceAssumedCurrent>
+inline event_t create(
+	device_t<DeviceAssumedCurrent>  device,
+	bool                            uses_blocking_sync = sync_by_busy_waiting,
+	bool                            records_timing     = do_record_timings,
+	bool                            interprocess       = not_interprocess)
+{
+	return create(device.id(), uses_blocking_sync, records_timing, interprocess);
+}
+
+} // namespace event_t
+
+
+// device_t methods
+
+template <bool AssumedCurrent>
+inline void device_t<AssumedCurrent>::synchronize(event_t& event)
+{
+	return synchronize_event(event.id());
+}
+
+template <bool AssumedCurrent>
+inline void device_t<AssumedCurrent>::synchronize(stream_t<detail::do_not_assume_device_is_current>& stream)
+{
+	return synchronize_stream(stream.id());
+}
+
+template <bool AssumedCurrent>
+inline stream_t<AssumedCurrent> device_t<AssumedCurrent>::default_stream() const noexcept
+{
+	// TODO: Perhaps support not-knowing our ID here as well, somehow?
+	return stream_t<AssumedCurrent>(id(), stream::default_stream_id);
+}
+
+template <bool AssumedCurrent>
+inline stream_t<detail::do_not_assume_device_is_current>
+device_t<AssumedCurrent>::create_stream(
+	bool                will_synchronize_with_default_stream,
+	stream::priority_t  priority)
+{
+	device::current::scoped_override_t<AssumedCurrent> set_device_for_this_scope(id_);
+	constexpr const auto take_ownership = true;
+	return stream::wrap(id(), stream::detail::create_on_current_device(
+		will_synchronize_with_default_stream, priority), take_ownership);
+}
+
+template <bool AssumedCurrent>
+inline event_t device_t<AssumedCurrent>::create_event(
+	bool          uses_blocking_sync,
+	bool          records_timing,
+	bool          interprocess)
+{
+	// The current implementation of event::create is not super-smart,
+	// but it's probably not worth it trying to improve just this function
+	return event::create<AssumedCurrent>(*this, uses_blocking_sync, records_timing, interprocess);
+}
+
+// event_t methods
+
+inline device_t<detail::do_not_assume_device_is_current>
+event_t::device() const { return cuda::device::get(device_id_); }
+
+
+// stream_t methods
+
+template <bool AssumesDeviceIsCurrent>
+inline device_t<detail::do_not_assume_device_is_current>
+stream_t<AssumesDeviceIsCurrent>::device() const { return cuda::device::get(device_id_); }
+
+template <bool AssumesDeviceIsCurrent>
+inline void stream_t<AssumesDeviceIsCurrent>::enqueue_t::wait(const event_t& event_)
+{
+#ifndef NDEBUG
+	if (event_.device_id() != device_id_) {
+		throw std::invalid_argument("Attempt to have a stream on CUDA device "
+			+ std::to_string(device_id_) + " wait for an event on another device ("
+			"device " + std::to_string(event_.device_id()) + ")");
+	}
+#endif
+
+	// Required by the CUDA runtime API; the flags value is
+	// currently unused
+	constexpr const unsigned int  flags = 0;
+
+	auto status = cudaStreamWaitEvent(stream_id_, event_.id(), flags);
+	throw_if_error(status,
+		std::string("Failed scheduling a wait for event ") + cuda::detail::ptr_as_hex(event_.id())
+		+ " on stream " + cuda::detail::ptr_as_hex(stream_id_)
+		+ " on CUDA device " + std::to_string(device_id_));
+
+}
+
+template <bool AssumesDeviceIsCurrent>
+inline event_t& stream_t<AssumesDeviceIsCurrent>::enqueue_t::event(event_t& existing_event)
+{
+#ifndef NDEBUG
+	if (existing_event.device_id() != device_id_) {
+		throw std::invalid_argument("Attempt to have a stream on CUDA device "
+			+ std::to_string(device_id_) + " wait for an event on another device ("
+			"device " + std::to_string(existing_event.device_id()) + ")");
+	}
+#endif
+	auto status = cudaEventRecord(existing_event.id(), stream_id_);
+	throw_if_error(status,
+		"Failed scheduling event " + cuda::detail::ptr_as_hex(existing_event.id()) + " to occur"
+		+ " on stream " + cuda::detail::ptr_as_hex(stream_id_)
+		+ " on CUDA device " + std::to_string(device_id_));
+	return existing_event;
+}
+
+template <bool AssumesDeviceIsCurrent>
+inline event_t stream_t<AssumesDeviceIsCurrent>::enqueue_t::event(
+    bool          uses_blocking_sync,
+    bool          records_timing,
+    bool          interprocess)
+{
+	event_t ev {event::create(device_id_, uses_blocking_sync, records_timing, interprocess)};
+	// so far, we've created an event which is not associated with this stream; we
+	// must specifically enqueue it:
+	this->event(ev);
+	return ev;
+}
+
+} // namespace cuda
+
+#endif /* MULTI_WRAPPER_IMPLS_HPP_ */
---
+++ thirdparty/cuda-api-wrappers/cuda/api/pointer.hpp
@@ -0,0 +1,88 @@
+/**
+ * @file pointer.hpp
+ *
+ * @brief A wrapper class for host and/or device pointers, allowing
+ * easy access to CUDA's pointer attributes.
+ *
+ * @note at the moment, this class is not used by other sections of the API
+ * wrappers; specifically, freestanding functions and methods returning
+ * pointers return raw `T*`'s rather than `pointer_t<T>`'s.
+ * This may change in the future.
+ *
+ * @todo Consider allowing for storing attributes within the class,
+ * lazily (e.g. with an std::optional).
+ */
+#pragma once
+#ifndef CUDA_API_WRAPPERS_POINTER_HPP_
+#define CUDA_API_WRAPPERS_POINTER_HPP_
+
+#include <cuda/api/types.hpp>
+#include <cuda/api/constants.hpp>
+#include <cuda/api/error.hpp>
+
+#include <cuda_runtime_api.h>
+
+namespace cuda {
+namespace memory {
+
+namespace pointer {
+
+struct attributes_t : cudaPointerAttributes {
+	bool on_host()    const  { return memoryType == cudaMemoryTypeHost; }
+	bool on_device()  const  { return memoryType == cudaMemoryTypeDevice; }
+	bool is_managed() const  { return isManaged; }
+};
+
+} // namespace pointer
+
+/**
+ * A convenience wrapper around a raw pointer "known" to the CUDA runtime
+ * and which thus has various kinds of associated information which this
+ * wrapper allows access to.
+ */
+template <typename T>
+class pointer_t {
+public: // getters and operators
+	T* get() const { return ptr_; }
+	operator T*() const { return ptr_; }
+
+public: // other non-mutators
+	pointer::attributes_t attributes() const
+	{
+		pointer::attributes_t the_attributes;
+		auto status = cudaPointerGetAttributes (&the_attributes, ptr_);
+		throw_if_error(status, "Failed obtaining attributes of pointer " + cuda::detail::ptr_as_hex(ptr_));
+		return the_attributes;
+	}
+	bool                is_on_host()     const  { return attributes().on_host();     }
+	bool                is_on_device()   const  { return attributes().on_device();   }
+	bool                is_managed()     const  { return attributes().is_managed();  }
+	cuda::device::id_t  device_id()      const  { return attributes().device;        }
+	T*                  get_for_device() const  { return attributes().devicePointer; }
+	T*                  get_for_host()   const  { return attributes().hostPointer;   }
+
+public: // constructors
+	pointer_t(T* ptr) noexcept : ptr_(ptr) { }
+	pointer_t(const pointer_t& other) noexcept = default;
+	pointer_t(pointer_t&& other) noexcept = default;
+
+protected: // data members
+	T* const ptr_;
+};
+
+namespace pointer {
+
+/**
+ * Wraps an existing pointer in a @ref pointer_t wrapper
+ *
+ * @param ptr a pointer - into either device or host memory -
+ * to be wrapped.
+ */
+template<typename T>
+inline pointer_t<T> wrap(T* ptr) noexcept { return pointer_t<T>(ptr); }
+
+} // namespace pointer
+} // namespace memory
+} // namespace cuda
+
+#endif /* CUDA_API_WRAPPERS_POINTER_HPP_ */
