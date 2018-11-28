073f50a3457a893ebd115bc15e700e88659a928e
Gawaboumga <yourihubaut@hotmail.com>
2018-11-28 18:03:14 +0100

    constexpr limits

--- gstl/utility/limits.cuh
+++ gstl/utility/limits.cuh
@@ -35,15 +35,15 @@ namespace gpu
 			static const int max_digits10 = 0;
 			static const type __min = detail::compute_min<type, digits, is_signed>::value;
 			static const type __max = is_signed ? type(type(~0) ^ __min) : type(~0);
-			GPU_DEVICE inline static type min() {return __min;}
-			GPU_DEVICE inline static type max() {return __max;}
-			GPU_DEVICE inline static type lowest() {return min();}
+			GPU_DEVICE GPU_CONSTEXPR inline static type min() {return __min;}
+			GPU_DEVICE GPU_CONSTEXPR inline static type max() {return __max;}
+			GPU_DEVICE GPU_CONSTEXPR inline static type lowest() {return min();}
 
 			static const bool is_integer = true;
 			static const bool is_exact = true;
 			static const int radix = 2;
-			GPU_DEVICE inline static type epsilon() {return type(0);}
-			GPU_DEVICE inline static type round_error() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type epsilon() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type round_error() {return type(0);}
 
 			static const int min_exponent = 0;
 			static const int min_exponent10 = 0;
@@ -54,10 +54,10 @@ namespace gpu
 			static const bool has_quiet_NaN = false;
 			static const bool has_signaling_NaN = false;
 			static const bool has_denorm_loss = false;
-			GPU_DEVICE inline static type infinity() {return type(0);}
-			GPU_DEVICE inline static type quiet_NaN() {return type(0);}
-			GPU_DEVICE inline static type signaling_NaN() {return type(0);}
-			GPU_DEVICE inline static type denorm_min() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type infinity() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type quiet_NaN() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type signaling_NaN() {return type(0);}
+			GPU_DEVICE GPU_CONSTEXPR inline static type denorm_min() {return type(0);}
 
 			static const bool is_iec559 = false;
 			static const bool is_bounded = true;
