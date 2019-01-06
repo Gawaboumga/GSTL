96f00b455c35c47632ec767977e09a9ee7c8922e
Gawaboumga <yourihubaut@hotmail.com>
2019-01-06 17:52:09 +0100

    Fix multiple definition

--- gstl/random/random_device.cu
+++ gstl/random/random_device.cu
@@ -5,22 +5,22 @@
 
 namespace gpu
 {
-	GPU_DEVICE double random_device::entropy() const noexcept
+	inline GPU_DEVICE double random_device::entropy() const noexcept
 	{
 		return 0.0;
 	}
 
-	GPU_DEVICE GPU_CONSTEXPR random_device::result_type random_device::max()
+	inline GPU_DEVICE GPU_CONSTEXPR random_device::result_type random_device::max()
 	{
 		return numeric_limits<result_type>::max();
 	}
 
-	GPU_DEVICE GPU_CONSTEXPR random_device::result_type random_device::min()
+	inline GPU_DEVICE GPU_CONSTEXPR random_device::result_type random_device::min()
 	{
 		return numeric_limits<result_type>::min();
 	}
 
-	GPU_DEVICE random_device::result_type random_device::operator()()
+	inline GPU_DEVICE random_device::result_type random_device::operator()()
 	{
 		return hash<offset_t>()(threadIdx.x + blockIdx.x * blockDim.x);
 	}
