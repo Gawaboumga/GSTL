19c1305fb4de5c5ff26cae9d4c5f7eaf077d0541
Gawaboumga <yourihubaut@hotmail.com>
2019-02-28 21:35:09 +0100

    Add swap_ranges_n

--- gstl/algorithms/swap.cu
+++ gstl/algorithms/swap.cu
@@ -1,9 +1,11 @@
 #include <gstl/algorithms/swap.cuh>
 
+#include <gstl/utility/swap.cuh>
+
 namespace gpu
 {
-	template <class ForwardIt1, class ForwardIt2>
-	GPU_DEVICE ForwardIt2 swap_ranges(block_t g, ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2)
+	template <class RandomIt1, class RandomIt2>
+	GPU_DEVICE RandomIt2 swap_ranges(block_t g, RandomIt1 first1, RandomIt1 last1, RandomIt2 first2)
 	{
 		auto len = distance(first1, last1);
 		offset_t thid = g.thread_rank();
@@ -14,10 +16,12 @@ namespace gpu
 			iter_swap(first1 + thid, first2 + thid);
 			thid += g.size();
 		}
+
+		return first2 + len;
 	}
 
-	template <class BlockTile, class ForwardIt1, class RandomIt2>
-	GPU_DEVICE ForwardIt2 swap_ranges(BlockTile g, ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2)
+	template <class BlockTile, class RandomIt1, class RandomIt2>
+	GPU_DEVICE RandomIt2 swap_ranges(BlockTile g, RandomIt1 first1, RandomIt1 last1, RandomIt2 first2)
 	{
 		auto len = distance(first1, last1);
 		offset_t thid = g.thread_rank();
@@ -28,6 +32,8 @@ namespace gpu
 			iter_swap(first1 + thid, first2 + thid);
 			thid += g.size();
 		}
+
+		return first2 + len;
 	}
 
 	template <class ForwardIt1, class ForwardIt2>
@@ -35,6 +41,46 @@ namespace gpu
 	{
 		using gpu::iter_swap;
 		while (first1 != last1)
+			iter_swap(first1++, first2++);
+
+		return first2;
+	}
+
+	template <class RandomIt1, class RandomIt2, class Size>
+	GPU_DEVICE RandomIt2 swap_ranges_n(block_t g, RandomIt1 first1, RandomIt2 first2, Size n)
+	{
+		offset_t thid = g.thread_rank();
+
+		while (thid < n)
+		{
+			using gpu::iter_swap;
+			iter_swap(first1 + thid, first2 + thid);
+			thid += g.size();
+		}
+
+		return first2 + n;
+	}
+
+	template <class BlockTile, class RandomIt1, class RandomIt2, class Size>
+	GPU_DEVICE RandomIt2 swap_ranges_n(BlockTile g, RandomIt1 first1, RandomIt2 first2, Size n)
+	{
+		offset_t thid = g.thread_rank();
+
+		while (thid < n)
+		{
+			using gpu::iter_swap;
+			iter_swap(first1 + thid, first2 + thid);
+			thid += g.size();
+		}
+
+		return first2 + n;
+	}
+
+	template <class ForwardIt1, class ForwardIt2, class Size>
+	GPU_DEVICE GPU_CONSTEXPR ForwardIt2 swap_ranges_n(ForwardIt1 first1, ForwardIt2 first2, Size n)
+	{
+		using gpu::iter_swap;
+		for (Size i = 0; i != n; ++i)
 			iter_swap(first1++, first2++);
 
 		return first2;
--- gstl/algorithms/swap.cuh
+++ gstl/algorithms/swap.cuh
@@ -7,14 +7,23 @@
 
 namespace gpu
 {
-	template <class ForwardIt1, class ForwardIt2>
-	GPU_DEVICE ForwardIt2 swap_ranges(block_t g, ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2);
+	template <class RandomIt1, class RandomIt2>
+	GPU_DEVICE RandomIt2 swap_ranges(block_t g, RandomIt1 first1, RandomIt1 last1, RandomIt2 first2);
 
-	template <class BlockTile, class ForwardIt1, class RandomIt2>
-	GPU_DEVICE ForwardIt2 swap_ranges(BlockTile g, ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2);
+	template <class BlockTile, class RandomIt1, class RandomIt2>
+	GPU_DEVICE RandomIt2 swap_ranges(BlockTile g, RandomIt1 first1, RandomIt1 last1, RandomIt2 first2);
 
 	template <class ForwardIt1, class ForwardIt2>
 	GPU_DEVICE GPU_CONSTEXPR ForwardIt2 swap_ranges(ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2);
+
+	template <class RandomIt1, class RandomIt2, class Size>
+	GPU_DEVICE RandomIt2 swap_ranges_n(block_t g, RandomIt1 first1, RandomIt2 first2, Size n);
+
+	template <class BlockTile, class RandomIt1, class RandomIt2, class Size>
+	GPU_DEVICE RandomIt2 swap_ranges_n(BlockTile g, RandomIt1 first1, RandomIt2 first2, Size n);
+
+	template <class ForwardIt1, class ForwardIt2, class Size>
+	GPU_DEVICE GPU_CONSTEXPR ForwardIt2 swap_ranges_n(ForwardIt1 first1, ForwardIt2 first2, Size n);
 }
 
 #include <gstl/algorithms/swap.cu>
